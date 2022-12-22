26a1894be408d7f044879501887f08dd90ac1f7d
aishanc <121129411+aishanc@users.noreply.github.com>
2022-12-22 20:32:11 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
-    container_name       = "tfstatefile"
+    resource_group_name  = "tamopstfstates"
+    storage_account_name = "nishstf"
+    container_name       = "statefiles"
     key                  = "dev.terraform.tfstate"
   }
 }
